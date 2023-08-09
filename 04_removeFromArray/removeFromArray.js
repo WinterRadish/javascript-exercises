c21889f9a5fb8f909302ed0002373688b978250d
Leng Tan <letng27@gmail.com>
2023-08-08 23:34:20 -0400

    Update removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...value) {
+    //make new array and only include elements that do not equal the values
+    let newArray = [];
+    for(const i of array){
+        if(!value.includes(i)){//check if i isn't in the value array
+            newArray.push(i);//add i to the new array
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
