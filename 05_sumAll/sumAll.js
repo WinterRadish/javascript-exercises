daa2435e4e328392e180154f00e7c619d8ac9ae1
Leng Tan <letng27@gmail.com>
2023-08-09 20:51:26 -0400

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,18 +8,16 @@ const sumAll = function(n1,n2) {
         n1 = n2, n2 = temp;
     }
 
-    /* solution using iteration
     let sum = 0;
     for(let i = n1; i <= n2; i++){
         sum+=i;
     }
     return sum;
-    */
 
     //solution using sum of integers sort of 
     //n*(a+l)/2, where n = # of integers, a = first term and l = last term
     //n = n2 - n1 + 1, n2 is the larger term, n1 is the smaller term, add 1 to account for 0 as an int
-    return(((n2-n1+1)*(n1+n2))/2);
+    //return(((n2-n1+1)*(n1+n2))/2);
 };
 
 // Do not edit below this line
